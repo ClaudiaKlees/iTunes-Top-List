1fb9f966f8e3ad7873a74d780bcf15bf0daf3714
Claudia Klees <Claudia Klees>
2017-01-13 10:19:41 -0500

    Switched out the mock JSON for actual data

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ var mediaControllers = angular.module('mediaControllers', ['ngAnimate']);
 
 mediaControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
     //controller for 'mediaControllers' which imports 'ListController'
-    $http.get('js/mock.json').success(function(data) {
+    $http.get('https://itunes.apple.com/us/rss/topalbums/limit=100/json').success(function(data) {
         $scope.medias = data.feed.entry;
         $scope.mediaOrder = 'Number';
         //sets data to use in html partial
